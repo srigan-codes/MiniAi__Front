7f917117c9bcc489e324e9c5486feaca169fcaf2
BoyManCoder <hani.mahdi623@gmail.com>
2024-08-22 15:39:43 +0100

    small card for small screens

---
+++ src/Components/ExecCardSmall.tsx
@@ -0,0 +1,50 @@
+
+
+interface Props{
+    imgURL: string,
+    name: string,
+    subname: string,
+    desc: string,
+}
+
+function ExecCardSmall({imgURL, name, subname, desc}: Props){
+    return (<>
+    <div className="small-card">
+      <button className="mail">
+        <svg
+          className="lucide lucide-mail"
+          strokeLinejoin="round"
+          strokeLinecap="round"
+          strokeWidth="2"
+          stroke="currentColor"
+          fill="none"
+          viewBox="0 0 24 24"
+          height="24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect rx="2" y="4" x="2" height="16" width="20"></rect>
+          <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+        </svg>
+      </button>
+      <div className="profile-pic">
+        <img src={imgURL} />
+      </div>
+      <div className="bottom">
+        <div className="content">
+          <span className="name">{name}<br/>{subname}</span>
+          <span className="about-me">
+            {desc}
+          </span>
+        </div>
+        <div className="bottom-bottom">
+          <div className="social-links-container"></div>
+          <button className="button">Contact Me</button>
+        </div>
+      </div>
+    </div>
+
+    </>)
+}
+
+export default ExecCardSmall;
--- src/Components/ExecPage.tsx
+++ src/Components/ExecPage.tsx
@@ -1,10 +1,15 @@
 import ExecCard from "./ExecCard";
+import ExecCardSmall from "./ExecCardSmall"
 
 function ExecPage(){
     return(<>
-    <ExecCard imgURL='[img]' name='Srigan Siva' subname="CFO" desc="[insert text]"/>
-    <ExecCard imgURL='[img]' name='Vedant Kansara' subname="CFO" desc="[insert text]"/>
+    <ExecCard imgURL='[img]' name='Srigan Siva' subname="CEO and Outreach Lead" desc="Srigan is a motivated high school student from Northview Heights Secondary. He spends his days learning about the ever-growing industry of artificial intelligence. Through emails and messages, Srigan has generated hundreds of connections to start this project. In his spare time, he loves to learn about quantum mechanics. Srigan is a passionate educator."/>
+    <ExecCard imgURL='[img]' name='Vedant Kansara' subname="Vedant- COO and Curriculum Developer" desc="Vedant is a motivated high school student from Northview Heights Secondary. He constantly spends time learning about the latest applications of artificial intelligence. In his spare time, he tutors children in math and helps refine their learning. Vedant is a passionate educator."/>
     <ExecCard imgURL='[img]' name='Hani Mahdi' subname="Lead Web Developer" desc="I'm a talented teenage front-end web developer who served as the lead developer at Mini Ai, where I recently spearheaded the design of our cutting-edge website."/>
+    <ExecCardSmall imgURL='[img]' name='Srigan Siva' subname="CEO and Outreach Lead" desc="Srigan is a motivated high school student from Northview Heights Secondary. He spends his days learning about the ever-growing industry of artificial intelligence. Through emails and messages, Srigan has generated hundreds of connections to start this project. In his spare time, he loves to learn about quantum mechanics. Srigan is a passionate educator."/>
+    <ExecCardSmall imgURL='[img]' name='Vedant Kansara' subname="Vedant- COO and Curriculum Developer" desc="Vedant is a motivated high school student from Northview Heights Secondary. He constantly spends time learning about the latest applications of artificial intelligence. In his spare time, he tutors children in math and helps refine their learning. Vedant is a passionate educator."/>
+    <ExecCardSmall imgURL='[img]' name='Hani Mahdi' subname="Lead Web Developer" desc="I'm a talented teenage front-end web developer who served as the lead developer at Mini Ai, where I recently spearheaded the design of our cutting-edge website."/>
+    
     </>
 )
 }
